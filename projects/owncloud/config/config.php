d0c6678ea24a22a7a424ac6c97207f870730d1bb
Hubert Pei <hp120374@gmail.com>
2020-03-05 10:57:14 -0800

    owncloud10.3.2 backup

---
+++ projects/owncloud/config/config.php
@@ -0,0 +1,46 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => 'ecSFmdGPLOTET6L2chPvjHxdrtNFT/',
+  'secret' => 'sOrd8Gd1YNZ9qZylpscBfLxEozdyuCHP3iyWxegmSU3hoygo',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => '192.168.1.129',
+    2 => 'www.thebestsolar.com',
+  ),
+  'datadirectory' => '/mnt/RAID5/NEW022020/DATA/owncloud',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbtype' => 'mysql',
+  'version' => '10.3.2.2',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => '/T6c07sU1eRBjcvZJwT+/5pZA77XEl',
+  'logtimezone' => 'UTC',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+  'instanceid' => 'ocsurejn5qsg',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'redis' => 
+  array (
+    'host' => '127.0.0.1',
+    'port' => '6379',
+  ),
+  'maintenance' => true,
+);
